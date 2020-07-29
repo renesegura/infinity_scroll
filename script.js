d3affe04527613a171e8ea6393dcb1dec13d3ff0
Rene <renesegura@yahoo.com>
2020-07-29 14:04:55 -0700

    added helper function to remove repetitive code

--- script.js
+++ script.js
@@ -8,20 +8,35 @@ const count = 5
 const apiKey = '6ObxMplYziQhmBhanzQ6UXh-Ye0LspIqp9Blu8273X4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=work out`;
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
 // Create Elements for links and photos, Add to DOM
 function displayPhotos() {
   // loop function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create empty <a> link tag to Unsplash
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    // item.setAttribute('href', photo.links.html);
+    // item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank'
+    });
     // Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
     // Put <img> inside <a>, then put both inside imageContainer
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     item.appendChild(img);
     imageContainer.appendChild(item);
 
